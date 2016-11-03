cf01e55f040aefbcbe6aa63bfc5da56f1fb405e3
robursu4u <robursu4u@gmail.com>
2016-11-02 23:00:12 -0700

    Fixed css after git merge bug, should be fine now.

--- src/github.com/robursu4u/main/static/dist/navigation.component.ts
+++ src/github.com/robursu4u/main/static/dist/navigation.component.ts
@@ -29,21 +29,18 @@ import { WelcomeComponent } from './welcome.component';
 
 <main>
 
-<div class="nav_container_outside">
-  
-  <div class="nav_container_inside">
-    <horizontal-separator class="nav_horizontal"></horizontal-separator>
-    <h1 class="nav">Welcome</h1>
-    <horizontal-separator class="nav_horizontal"></horizontal-separator>
-    <h1 class="nav">Listen Now</h1>
-    <horizontal-separator class="nav_horizontal"></horizontal-separator>
-    <h1 class="nav">Login</h1>
-    <horizontal-separator class="nav_horizontal"></horizontal-separator>
-    <h1 class="nav">Contact</h1>
-    <horizontal-separator class="nav_horizontal"></horizontal-separator>
-  </div>
-
+<<<<<<< HEAD
+<div class="nav_container">
+  <h1 class="nav">Welcome</h1>
+  <horizontal-separator class="nav_horizontal"></horizontal-separator>
+  <h1 class="nav">Listen Now</h1>
+  <horizontal-separator class="nav_horizontal"></horizontal-separator>
+  <h1 class="nav">Login</h1>
+  <horizontal-separator class="nav_horizontal"></horizontal-separator>
+  <h1 class="nav">Contact</h1>
+  <horizontal-separator class="nav_horizontal"></horizontal-separator>
 </div>
+
 
 <div class="everything_else_container">
 
@@ -60,38 +57,75 @@ import { WelcomeComponent } from './welcome.component';
 </div>
 
 
+
+</main>
+  `,
+  styles: [`
+    .nav_container{
+=======
+<div class="nav_container_outside">
+  <div class="nav_containter_inside">
+    <h1 class="nav">Welcome</h1>
+    <horizontal-separator class="nav_horizontal"></horizontal-separator>
+    <h1 class="nav">Listen Now</h1>
+    <horizontal-separator class="nav_horizontal"></horizontal-separator>
+    <h1 class="nav">Login</h1>
+    <horizontal-separator class="nav_horizontal"></horizontal-separator>
+    <h1 class="nav">Contact</h1>
+    <horizontal-separator class="nav_horizontal"></horizontal-separator>
+  </div>
+</div>
+
+
+
+<div class="everything_else_container">
+  <div class="welcome_container"><welcome-component></welcome-component></div>
+  <div class="horizontal_main_page"><horizontal-separator></horizontal-separator></div>
+  <podcast-list></podcast-list>
+  <div class="horizontal_main_page"><horizontal-separator></horizontal-separator></div>
+  <login-module></login-module>
+</div>
+
 </main>
   `,
   styles: [`
     .nav_container_outside{
+>>>>>>> bba013f1b0b22390feb8cad49c4be785d98e26e3
       border-right-style: groove;
       border-color: #0dba83;
       border-width: 10px;
       height: 100%; 
       position: fixed; 
       z-index: 1;
-      top: 0;
+<<<<<<< HEAD
+      top: 10%;
       left: 0;
-      width: 200px;
+      width: 12%;
       padding-left: 10px;
       padding-right: 10px;
       background-color: #272b30;
     }
-    .nav_container_inside{
-      position: fixed;
-      height: 100%;
+    .everything_else_container{
+      position: relative;
+      padding: 40px;
+      width: 100%;
+=======
+      top: 0;
+      left: 0;
+      margin-right: 40px;
       padding-left: 10px;
-      padding-right: 10px;
-      left: 0; 
-      top: 20%;
+      background-color: #272b30;
+    }
+    .nav_containter_inside {
+      position: relative;
+      margin-right: 40px; 
+      top: 15%;
     }
     .everything_else_container{
       position: absolute;
+      left: 280px;
       width: 60%;
-      top: 0;
-      left: 200px;
-      padding-left: 60px;
-      background-color: #272b30;
+>>>>>>> bba013f1b0b22390feb8cad49c4be785d98e26e3
     }
     .nav{
       font-family: 'Noir', Verdana, Arial;
@@ -105,8 +139,11 @@ import { WelcomeComponent } from './welcome.component';
     .horizontal_main_page{
       padding-top: 70px;
       padding-bottom: 70px;
+<<<<<<< HEAD
+=======
       position: relative;
       left: 0;
+>>>>>>> bba013f1b0b22390feb8cad49c4be785d98e26e3
     }
     .nav_horizontal{
       position: relative;
@@ -118,12 +155,16 @@ import { WelcomeComponent } from './welcome.component';
       border-color: #0dba83;
       position: fixed;
       left: 0;
+<<<<<<< HEAD
       width: 98%;
+=======
       width: 96%;
+>>>>>>> bba013f1b0b22390feb8cad49c4be785d98e26e3
       background-color: #272b30;
       z-index: 1;
       top: 0;
       left: 0;
+
     }
   `]
 })
