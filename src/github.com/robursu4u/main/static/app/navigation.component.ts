bba013f1b0b22390feb8cad49c4be785d98e26e3
robursu4u <robursu4u@gmail.com>
2016-11-02 21:43:28 -0700

    More css manipulation so site looks good on different resolutions

--- src/github.com/robursu4u/main/static/app/navigation.component.ts
+++ src/github.com/robursu4u/main/static/app/navigation.component.ts
@@ -29,55 +29,54 @@ import { WelcomeComponent } from './welcome.component';
 
 <main>
 
-<div class="nav_container">
-  <h1 class="nav">Welcome</h1>
-  <horizontal-separator class="nav_horizontal"></horizontal-separator>
-  <h1 class="nav">Listen Now</h1>
-  <horizontal-separator class="nav_horizontal"></horizontal-separator>
-  <h1 class="nav">Login</h1>
-  <horizontal-separator class="nav_horizontal"></horizontal-separator>
-  <h1 class="nav">Contact</h1>
-  <horizontal-separator class="nav_horizontal"></horizontal-separator>
+<div class="nav_container_outside">
+  <div class="nav_containter_inside">
+    <h1 class="nav">Welcome</h1>
+    <horizontal-separator class="nav_horizontal"></horizontal-separator>
+    <h1 class="nav">Listen Now</h1>
+    <horizontal-separator class="nav_horizontal"></horizontal-separator>
+    <h1 class="nav">Login</h1>
+    <horizontal-separator class="nav_horizontal"></horizontal-separator>
+    <h1 class="nav">Contact</h1>
+    <horizontal-separator class="nav_horizontal"></horizontal-separator>
+  </div>
 </div>
+
 
 
 <div class="everything_else_container">
-
   <div class="welcome_container"><welcome-component></welcome-component></div>
-
   <div class="horizontal_main_page"><horizontal-separator></horizontal-separator></div>
-
   <podcast-list></podcast-list>
-
   <div class="horizontal_main_page"><horizontal-separator></horizontal-separator></div>
-  
   <login-module></login-module>
-
 </div>
-
-
 
 </main>
   `,
   styles: [`
-    .nav_container{
+    .nav_container_outside{
       border-right-style: groove;
       border-color: #0dba83;
       border-width: 10px;
       height: 100%; 
       position: fixed; 
       z-index: 1;
-      top: 10%;
+      top: 0;
       left: 0;
-      width: 12%;
+      margin-right: 40px;
       padding-left: 10px;
-      padding-right: 10px;
       background-color: #272b30;
     }
-    .everything_else_container{
+    .nav_containter_inside {
       position: relative;
-      padding: 40px;
-      width: 100%;
+      margin-right: 40px; 
+      top: 15%;
+    }
+    .everything_else_container{
+      position: absolute;
+      left: 280px;
+      width: 60%;
     }
     .nav{
       font-family: 'Noir', Verdana, Arial;
@@ -91,6 +90,8 @@ import { WelcomeComponent } from './welcome.component';
     .horizontal_main_page{
       padding-top: 70px;
       padding-bottom: 70px;
+      position: relative;
+      left: 0;
     }
     .nav_horizontal{
       position: relative;
@@ -102,7 +103,7 @@ import { WelcomeComponent } from './welcome.component';
       border-color: #0dba83;
       position: fixed;
       left: 0;
-      width: 98%;
+      width: 96%;
       background-color: #272b30;
       z-index: 1;
       top: 0;
